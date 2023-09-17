ef76b2fb3703c63182b8c01dd6950f3297bae814
Captain1014 <leahchung99@gmail.com>
2023-09-16 21:35:08 -0400

    Update script.js
    Fixed typos

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ let adjustX = 6;
 let adjustY = 0;
 ctx.lineWidth = 3;
 
-// handle mouse
+// Handle mouse
 const mouse = {
     x: null,
     y: null,
-    radius: 150
-}
+    radius: 150,
+};
 
-window.addEventListener('mousemove', function(event){
+window.addEventListener('mousemove', function(event) {
     mouse.x = event.x;
     mouse.y = event.y;
 });
@@ -22,56 +22,53 @@ window.addEventListener('mousemove', function(event){
 ctx.fillStyle = 'white';
 ctx.font = '25px Verdana';
 ctx.fillText('안녕', 0, 30);
-const textCoordinates = ctx.getImageData(0,0,100,100);
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
 
 class Particle {
-    constructor(x,y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.size = 3;
         this.baseX = this.x;
         this.baseY = this.y;
-        this.density = (Math.random()*8)+1;
+        this.density = (Math.random() * 8) + 1;
         this.distance;
     }
-    draw(){
-        ctx.fillstyle = 'rgba(255,255,255,0.8)';
-        ctx.strokeStyle = 'rgba(34,147,214,1)';
+
+    draw() {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        ctx.strokeStyle = 'rgba(34, 147, 214, 1)';
         ctx.beginPath();
 
-        if(this.distance < mouse.radius - 5){
+        if (this.distance < mouse.radius - 5) {
             this.size = 13;
             ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
             ctx.stroke();
             ctx.closePath();
             ctx.beginPath();
-            ctx.arc(this.x-3, this.y-3, this.size/2.5, 0, Math.PI * 2);
-            ctx.arc(this.x+7, this.y-1, this.size/3.5, 0, Math.PI * 2);
-        }
-
-        else if(this.distance <= mouse.radius){
+            ctx.arc(this.x - 3, this.y - 3, this.size / 2.5, 0, Math.PI * 2);
+            ctx.arc(this.x + 7, this.y - 1, this.size / 3.5, 0, Math.PI * 2);
+        } else if (this.distance <= mouse.radius) {
             this.size = 10;
             ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
             ctx.stroke();
             ctx.closePath();
             ctx.beginPath();
-            ctx.arc(this.x-2, this.y-2, this.size/3, 0, Math.PI * 2);
-           
-        } else{
-
+            ctx.arc(this.x - 2, this.y - 2, this.size / 3, 0, Math.PI * 2);
+        } else {
             this.size = 8;
             ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
             ctx.stroke();
             ctx.closePath();
             ctx.beginPath();
-            ctx.arc(this.x-1, this.y-1, this.size/3, 0, Math.PI * 2);
-
+            ctx.arc(this.x - 1, this.y - 1, this.size / 3, 0, Math.PI * 2);
         }
-     
+
         ctx.closePath();
-        ctx.fill(); 
+        ctx.fill();
     }
-    update(){
+
+    update() {
         let dx = mouse.x - this.x;
         let dy = mouse.y - this.y;
         let distance = Math.sqrt(dx * dx + dy * dy);
@@ -83,46 +80,44 @@ class Particle {
         let directionX = forceDirectionX * force * this.density;
         let directionY = forceDirectionY * force * this.density;
 
-        if(distance < mouse.radius){
-            this.x -= directionX;
-            this.y -= directionY;
-        }
-        else {
-            if(this.x !== this.baseX){
+        if (distance < mouse.radius) {
+            this.x += directionX;
+            this.y += directionY;
+        } else {
+            if (this.x !== this.baseX) {
                 let dx = this.x - this.baseX;
-                this.x -= dx/10;
+                this.x -= dx / 10;
             }
-            if(this.y !== this.baseY){
+            if (this.y !== this.baseY) {
                 let dy = this.y - this.baseY;
-                this.y -= dy/10;
+                this.y -= dy / 10;
             }
         }
     }
 }
 
-function init(){
+function init() {
     particleArray = [];
-    for (let y = 0, y2 = textCoordinates.height; y < y2; y++){
-        for(let x = 0, x2 = textCoordinates.width; x < x2; x++){
-            if(textCoordinates.data[(y * 4 * textCoordinates.width) + (x*4)+3] > 128){
+    for (let y = 0; y < textCoordinates.height; y++) {
+        for (let x = 0; x < textCoordinates.width; x++) {
+            if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 128) {
                 let positionX = x + adjustX;
                 let positionY = y + adjustY;
                 particleArray.push(new Particle(positionX * 20, positionY * 20));
             }
         }
     }
-
 }
-init();
-console.log(particleArray);
 
-function animate(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    for(let i = 0; i < particleArray.length; i++){
+init();
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particleArray.length; i++) {
         particleArray[i].draw();
         particleArray[i].update();
     }
     requestAnimationFrame(animate);
 }
-animate();
 
+animate();
